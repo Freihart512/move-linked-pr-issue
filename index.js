f77fa34dd6036b9847e4bff75ca38a0efa83048b
Carlos Gonzalez <carlosfabian512@gmail.com>
2023-09-24 16:45:05 -0600

    test

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
+const payload = JSON.stringify(github.context.payload, undefined, 2)
 const graphqlApi = require('./graphql');
+var get = require('lodash.get');
 const { getProjectInfoByNameWithUser, getProjectInfoByNameWithOrg } = require('./graphql/queries');
 
 
@@ -9,21 +12,44 @@ async function run() {
     const projectName = core.getInput('project_name');
     const userName = core.getInput('user_name');
     const orgName = core.getInput('org_name');
-    const targetCol = '' // core.getInput('targetCol');
+    const targetColName = core.getInput('targetCol');
     const githubToken = core.getInput('github_token');
     const personalToken = core.getInput('personal_token');
 
-    const graphqlInstance = new graphqlApi(personalToken);
+    const token = personalToken || githubToken;
+
+    const graphqlInstance = new graphqlApi(token);
     
     
     core.info(`getting project info ...`);
     const getProjectQuery = orgName ? getProjectInfoByNameWithOrg(projectName, userName) : getProjectInfoByNameWithUser(projectName, userName) 
     core.info(getProjectQuery);
-    const project = await graphqlInstance.query(getProjectQuery)
+    const reponse = await graphqlInstance.query(`
+    mutation {
+      updateProjectV2ItemFieldValue(
+        input: {projectId: "PVT_kwHOANGp0s4ATT8y", itemId: "I_kwDOJ4bThM5tkZGt", fieldId: "PVTSSF_lAHOANGp0s4ATT8yzgMVbsY", value: {singleSelectOptionId: "47fc9ee4"}}
+      ) {
+        projectV2Item {
+          id
+        }
+      }
+    }`)
+    //{"user":{"projectsV2":{"edges":[{"node":{"id":"PVT_kwHOANGp0s4ATT8y","title":"Scrapper Infracciones","field":{"__typename":"ProjectV2SingleSelectField","id":"PVTSSF_lAHOANGp0s4ATT8yzgMVbsY","options":[{"id":"c1a353c4","name":"Backlog"},{"id":"f75ad846","name":"Todo"},{"id":"47fc9ee4","name":"In Progress"},{"id":"6d23dd3c","name":"In code Review"},{"id":"98236657","name":"Done"}]}}}]}}}
+    // const p =  get(reponse, 'user.projectsV2,edges[0].node');
+    // const project = {
+    //   id: p.id,
+    //   title: p.title,
+    //   colsId: p.field,
+    //   colsOptions: p.field.options
+    // }
 
-    core.info(`Project ${JSON.stringify(project)}`);
-    core.debug(project);
-    core.debug(JSON.stringify(project));
+    // const targetCol = project.colsOptions.filter(col => col.name === targetColName);
+
+
+
+    core.info(`Project ${JSON.stringify(reponse)}`);
+    core.debug(reponse);
+    core.debug(JSON.stringify(reponse));
   } catch (error) {
     core.info(`error ${error} ...`);
     core.setFailed(error.message);
